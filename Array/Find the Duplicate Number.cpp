28e630fc9d6b1f76cc5579b612b47076e690dbf9
abhishek170602 <abhitechy17@gmail.com>
2024-07-16 19:44:34 +0530

    Optimized approach added

--- Array/Find the Duplicate Number.cpp
+++ Array/Find the Duplicate Number.cpp
@@ -1,16 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// First Approach
+
+// class Solution {
+// public:
+//     int findDuplicate(vector<int>& nums) {
+//         unordered_map<int, int> freq;
+//         for(int i=0;i<nums.size();i++){
+//             freq[nums[i]]++;
+//         }
+//         for(auto it: freq){
+//             if(it.second>=2) return it.first;
+//         }
+//         return -1;
+//     }
+// };
+
+// Better Approach
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        unordered_map<int, int> freq;
-        for(int i=0;i<nums.size();i++){
-            freq[nums[i]]++;
+        int slow = nums[0];
+        int fast = nums[0];
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+        while(slow!=fast){
+            slow = nums[slow];
+            fast = nums[nums[fast]];
         }
-        for(auto it: freq){
-            if(it.second>=2) return it.first;
+        slow = nums[0];
+        while(slow!=fast){
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return -1;
+        return slow;
     }
 };
