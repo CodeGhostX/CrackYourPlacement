d61ccb3766310b682798e40b6a69c0d1b95f0719
abhishek170602 <abhitechy17@gmail.com>
2024-07-16 15:20:20 +0530

    Find the duplicate number

---
+++ Array/Find the Duplicate Number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        unordered_map<int, int> freq;
+        for(int i=0;i<nums.size();i++){
+            freq[nums[i]]++;
+        }
+        for(auto it: freq){
+            if(it.second>=2) return it.first;
+        }
+        return -1;
+    }
+};
