e86b2846dd3db62452de103f70851370fec1506f
abhishek170602 <abhitechy17@gmail.com>
2024-07-17 07:35:48 +0530

    Find all duplicates

---
+++ Array/Find All Duplicates in an Array.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++){
+            int idx = abs(nums[i])-1;
+            if(nums[idx]>0) nums[idx] *= -1;
+            else ans.push_back(idx+1);
+        }
+        return ans;
+    }
+};
