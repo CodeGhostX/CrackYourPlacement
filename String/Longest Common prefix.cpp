7ed36c44fe8eebfe460dcd5975258682f9af6c87
abhishek170602 <abhitechy17@gmail.com>
2024-07-18 23:54:08 +0530

    Longest common prefix optimized

--- String/Longest Common prefix.cpp
+++ String/Longest Common prefix.cpp
@@ -1,21 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// class Solution {
+// public:
+//     string longestCommonPrefix(vector<string>& strs) {
+//         int j = 0;
+//         string ans = "";
+//         while(j<strs[0].size()){
+//             int i = 1;
+//             while(i<strs.size()){
+//                 if(j==strs[i].size() || strs[i][j]!=strs[0][j]) break;
+//                 i++;
+//             }
+//             if(i!=strs.size()) return ans;
+//             else ans += strs[0][j];
+//             j++;
+//         }
+//         return ans;
+//     }
+// };
+
+// Optimized Approach
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        int j = 0;
-        string ans = "";
-        while(j<strs[0].size()){
-            int i = 1;
-            while(i<strs.size()){
-                if(j==strs[i].size() || strs[i][j]!=strs[0][j]) break;
-                i++;
-            }
-            if(i!=strs.size()) return ans;
-            else ans += strs[0][j];
-            j++;
+        sort(strs.begin(), strs.end());
+        int i = 0;
+        while(i<strs[0].size()){
+            if(strs[0][i]!=strs.back()[i]) return strs[0].substr(0, i);;
+            i++;
         }
-        return ans;
+        return strs[0].substr(0, i+1);
     }
 };
