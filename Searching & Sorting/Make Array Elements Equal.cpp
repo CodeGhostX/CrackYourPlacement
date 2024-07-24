ce3a36e245a8d3158550fde698e66b36e7cebd17
abhishek170602 <abhitechy17@gmail.com>
2024-07-24 18:45:02 +0530

    Make array elements equal

---
+++ Searching & Sorting/Make Array Elements Equal.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#include <bits/stdc++.h>
+int findMinimumCost(vector<int> &arr, int n)
+{
+    sort(arr.begin(), arr.end());
+    int median;
+    if(n%2==0){
+        int idx = n/2;
+        median = (arr[idx]+arr[idx-1])/2;
+    }
+    else median = arr[n/2];
+    int ans = 0;
+    for(int i=0;i<n;i++){
+        ans += abs(arr[i]-median);
+    }
+    return ans;
+}
